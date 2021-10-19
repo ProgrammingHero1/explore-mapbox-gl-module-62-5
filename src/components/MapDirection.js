0381f4a87dfd563e5a166002da6cc4bff8a08797
Programming Hero <programming.hero1@gmail.com>
2021-10-19 07:30:03 -0500

    explore mapbox stuff

---
+++ src/components/MapDirection.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
+
+mapboxgl.accessToken = 'pk.eyJ1IjoicHJvZ3JhbW1pbmdoZXJvIiwiYSI6ImNrdXc3OG04MzFsbHgzM255ZnU0NDRtbmsifQ.sEGTkRu5uz5PBIoho1An0w';
+
+const MapDirection = () => {
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [90.389099, 23.791599],
+            zoom: 13
+        });
+
+        map.addControl(
+            new MapboxDirections({
+                accessToken: mapboxgl.accessToken
+            }),
+            'top-left'
+        );
+    }, [])
+    return (
+        <div>
+            <div id="map"></div>
+        </div>
+    );
+};
+
+export default MapDirection;
